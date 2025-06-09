f0609701887a539ad5186f1f4574fad1e1f7a6dd
SUMIT JADAV <jadavsumit987@gmail.com>
2025-06-10 00:19:58 +0530

    User model creation

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const usersSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", usersSchema);
+
+export default User;
