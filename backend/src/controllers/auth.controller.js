f0824ed196cded86e50a0ca5b569320e96fea297
SUMIT JADAV <jadavsumit987@gmail.com>
2025-06-10 01:09:31 +0530

    Sign up route complete

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,51 @@
-export const signup = (req, res) => {
-  res.send("Sign Up");
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // hash password
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 character long" });
+    }
+    const user = await User.findOne({ email });
+
+    if (user) {
+      res.status(400).json({ message: "User already exist!!" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashPass,
+    });
+    if (newUser) {
+      // generate JWT token
+      generateToken(newUser._id, res);
+      await newUser.save();
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log(`Error in signup controller ${error}`);
+    return res.status(500).json({ message: "Server Error" });
+  }
 };
 
 export const login = (req, res) => {
