0c58fe351c946c87870c389047f67159e86f48ab
SUMIT JADAV <jadavsumit987@gmail.com>
2025-06-10 00:21:58 +0530

    Use of env and creation of route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
-
+import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
-
+import { connectDB } from "./lib/db.js";
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5001;
 
-app.use("api/auth", authRouter);
+app.use("/api/auth", authRouter);
 
-app.listen(5001, () => {
-  console.log("http://localhost:5001/");
+app.listen(PORT, () => {
+  console.log(`http://localhost:/${PORT}`);
+  connectDB();
 });
