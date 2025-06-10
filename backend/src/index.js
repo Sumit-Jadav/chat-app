5b29f215bff94c0e3d6c0d25477b4a8d586793fd
SUMIT JADAV <jadavsumit987@gmail.com>
2025-06-10 19:49:33 +0530

    Configuration of some library

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
