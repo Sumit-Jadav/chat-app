90acc05fc472a82ac4246e5a0c448a79340c0ce1
SUMIT JADAV <jadavsumit987@gmail.com>
2025-07-07 17:01:56 +0530

    Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-import daisyui from "daisyui";
+import daisyui from "daisyui/index.js";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -8,6 +8,36 @@ export default {
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "bumblebee", "emerald"],
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+    ],
   },
 };
